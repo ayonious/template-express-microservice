bcc3f5982d7ffce48c265a5badf911cb08d2f83c
Nahiyan Kamal <noyanayihan@gmail.com>
2020-06-05 19:05:34 +0200

    auto generated message

--- src/controllers/server.js
+++ src/controllers/server.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const app = express();
 
+const { param } = require("express-validator");
 const { sum } = require("../utils/utils");
 
-app.get("/", function (req, res) {
+app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.get("/sum", function (req, res) {
+app.get("/sum", [param("val").isString()], (req, res) => {
   const param = +req.query.val;
   console.log("param is", param);
   res.send(`sum of [1 to ${param}] is ${sum(param)}`);
